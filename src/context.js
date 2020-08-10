1eb10a4d50b15298757a3ee6c4ecab9baeaeee2b
Arthur <Strevitz@github.com>
2020-08-10 22:30:44 +0200

    commit styled hero

--- src/context.js
+++ src/context.js
@@ -34,9 +34,15 @@ class RoomProvider extends Component {
     return tempItems;
   }
 
+  getRoom = (slug) => {
+    let tempRooms = [...this.state.rooms];
+    const room = tempRooms.find((room) => room.slug === slug);
+    return room;
+  };
+
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state }}>
+      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
         {this.props.children}
       </RoomContext.Provider>
     );
--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,7 +1,55 @@
 import React, { Component } from "react";
+import defaultBg from "../images/room-1.jpeg";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
+import { RoomContext } from "../context";
+import styledHero from "../components/StyledHero";
 
 export default class SingleRoom extends Component {
+  constructor(props) {
+    super(props);
+    console.log(this.props);
+    this.state = {
+      slug: this.props.match.params.slug,
+      defaultBg,
+    };
+  }
+  static contextType = RoomContext;
+  componentDidMount() {}
+
   render() {
-    return <div>hello from single room</div>;
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+    if (!room) {
+      return (
+        <div className="error">
+          <h3>no such room coluld be found...</h3>
+          <Link to="/rooms" className="btn-primary">
+            back to rooms
+          </Link>
+        </div>
+      );
+    }
+    const {
+      name,
+      desription,
+      capasity,
+      size,
+      price,
+      extras,
+      breakfast,
+      pets,
+      images,
+    } = room;
+    return (
+      <Hero hero="roomsHero">
+        <Banner title={`${name} room`}>
+          <Link to="rooms" className="btn-primary">
+            back to rooms
+          </Link>
+        </Banner>
+      </Hero>
+    );
   }
 }
