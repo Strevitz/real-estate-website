8dc08d31beeed3d339c0d77fe5630bda730e8002
Arthur <Strevitz@github.com>
2020-08-26 13:25:21 +0200

    commit

--- src/components/ApartmentsList.js
+++ src/components/ApartmentsList.js
@@ -5,7 +5,7 @@ export default function RoomlList({ rooms }) {
   if (rooms.length === 0) {
     return (
       <div className="empty-search">
-        <h3>unfortunately no rooms matched your search parameters</h3>
+        <h3>unfortunately no apartments matched your search parameters</h3>
       </div>
     );
   }
