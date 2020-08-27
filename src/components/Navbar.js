0e1fa05c3bb9fcf159cd06b77be787f85d65a835
Arthur <Strevitz@github.com>
2020-08-27 21:13:14 +0200

    navbar commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,12 +7,13 @@ export default class Navbar extends Component {
   state = {
     isOpen: false,
   };
+
   handleToggle = () => {
     this.setState({ isOpen: !this.state.isOpen });
   };
 
-  handleLinks = () => {
-    this.setState({ isOpen: !this.state.isOpen });
+  closeMenu = () => {
+    this.setState({ isOpen: false });
   };
 
   render() {
@@ -21,20 +22,25 @@ export default class Navbar extends Component {
         <div className="nav-center">
           <div className="nav-header">
             <Link to="/">
-              <img src={logo} alt="beach resort" />
+              <img src={logo} alt="Propertie" />
             </Link>
             <button
+              onClick={this.handleToggle}
               type="button"
               className="nav-btn"
-              onClick={this.handleToggle}
             >
-              <Hamburger rounded={true} className="nav-icon" />
+              <Hamburger
+                size={18}
+                duration={0.5}
+                rounded
+                className="nav-icon"
+              />
             </button>
           </div>
           <ul
             className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}
           >
-            <li onClick={this.handleLinks}>
+            <li onClick={this.closeMenu}>
               <NavLink
                 to="/"
                 exact
@@ -43,7 +49,7 @@ export default class Navbar extends Component {
                 Home
               </NavLink>
             </li>
-            <li onClick={this.handleLinks}>
+            <li onClick={this.closeMenu}>
               <NavLink
                 to="/apartments"
                 exact
@@ -52,7 +58,7 @@ export default class Navbar extends Component {
                 Apartments
               </NavLink>
             </li>
-            <li onClick={this.handleLinks}>
+            <li onClick={this.closeMenu}>
               <NavLink
                 to="/about"
                 exact
@@ -61,7 +67,7 @@ export default class Navbar extends Component {
                 About Propriété
               </NavLink>
             </li>
-            <li onClick={this.handleLinks}>
+            <li onClick={this.closeMenu}>
               <NavLink
                 to="/contact"
                 exact
